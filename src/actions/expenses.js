ce81bcb02dfbb958cda148d7c0cf7850ce88e156
Justin Somerville <justinsom@gmail.com>
2018-04-20 17:57:44 +0200

    Add private routes

--- src/actions/expenses.js
+++ src/actions/expenses.js
@@ -51,9 +51,7 @@ export const editExpense = (id, updates) => ({
 
 export const startEditExpense = (id, updates) => {
   return (dispatch) => {
-    return database.ref(`expenses/${id}`).update({
-      ...updates
-    })
+    return database.ref(`expenses/${id}`).update(updates)
       .then(() => {
         dispatch(editExpense(id, updates));
       });
